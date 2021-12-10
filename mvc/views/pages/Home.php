c9a0fb93c185214360f54a2fec215a695e547f1c
fairy186 <huy.nx.60cntt@ntu.edu.vn>
2021-12-10 17:26:51 +0700

    Update Home.php

--- mvc/views/pages/Home.php
+++ mvc/views/pages/Home.php
@@ -1,2 +1,13 @@
 <?php
-print_r($data["dLap"]);
+$dLap = $data["dLap"];
+for ($i=0; $i < count($dLap); $i++) { 
+     $dLap1=$dLap[$i];
+     for ($j=0; $j < count($dLap1)/2; $j++) { 
+          echo $dLap1[$j]." ";
+     }
+     
+     echo "</br>";
+}
+?>
+
+<h1></h1>
