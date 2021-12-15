7c88f8c18f29b0ba7af6e3a28326339424cab1d7
fairy186 <huy.nx.60cntt@ntu.edu.vn>
2021-12-15 15:53:31 +0700

    un comment JS

--- mvc/views/layout/Layout1.php
+++ mvc/views/layout/Layout1.php
@@ -10,7 +10,6 @@
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <!-- <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script> -->
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
@@ -40,12 +39,12 @@
           <?php
           require_once "./mvc/views/pages/" . $data['page'] . ".php"
           ?>
-
      </div>
      <div id="footer">
           <h1> Footer</h1>
      </div>
      <script src="../public/App.js">
+
      </script>
 </body>
 
--- public/App.js
+++ public/App.js
@@ -1,45 +1,41 @@
+function validate(fieldname, idmessage) {
+  $("input[name=" + fieldname + "]").keyup(function () {
+    var val = $(this).val();
+    var name = $(this).attr("placeholder");
+    var mess = $(".mess");
+    var messblue = $(".blue");
+    $.post(
+      "../Ajax/Check/" + fieldname + "",
+      {
+        val: val,
+      },
+      function (data) {
+        var d = JSON.parse(data);
 
-// function validate(fieldname, idmessage) {
-//   $("input[name=" + fieldname + "]").keyup(function () {
-//     var val = $(this).val();
-//     var name = $(this).attr("placeholder");
-//     var mess = $(".mess");
-//     var messblue = $(".blue");
-//     $.post(
-//       "../Ajax/Check/" + fieldname + "",
-//       {
-//         val: val,
-//       },
-//       function (data) {
-//         var d = JSON.parse(data);
+        if (d[0] == 1) {
+          $("#" + idmessage).html(
+            "<i class='bi bi-check2-circle'></i> " + name + " " + d[1]
+          );
+          $("#" + idmessage).css("color", "blue");
+          $("#" + idmessage).addClass("blue");
+          $("#" + idmessage).removeClass("red");
+        } else {
+          $("#" + idmessage).html(
+            "<i class='bi bi-x-circle'></i> " + name + " " + d[1]
+          );
+          $("#" + idmessage).css("color", "red");
+          $("#" + idmessage).addClass("red");
+          $("#" + idmessage).removeClass("blue");
+        }
+      }
+    );
+    if (mess.length == messblue.length)
+      $("button[name='sm']").removeClass("disabled");
+    else $("button[name='sm']").addClass("disabled");
+  });
+}
 
-//         if (d[0] == 1) {
-//           $("#" + idmessage).html(
-//             "<i class='bi bi-check2-circle'></i> " + name + " " + d[1]
-//           );
-//           $("#" + idmessage).css("color", "blue");
-//           $("#" + idmessage).addClass("blue");
-//           $("#" + idmessage).removeClass("red");
-//         } else {
-//           $("#" + idmessage).html(
-//             "<i class='bi bi-x-circle'></i> " + name + " " + d[1]
-//           );
-//           $("#" + idmessage).css("color", "red");
-//           $("#" + idmessage).addClass("red");
-//           $("#" + idmessage).removeClass("blue");
-//         }
-//       }
-//     );
-   
-//     if (mess.length == messblue.length)
-//       $("button[name='sm']").removeClass("disabled");
-//     else
-//       $("button[name='sm']").addClass("disabled");
-      
-//   });
-// }
-
-// $(document).ready(function () {
-//   validate("ma", "IDmessage");
-//   validate("ten", "NameMessage");
-// });
+$(document).ready(function () {
+  validate("ma", "IDmessage");
+  validate("ten", "NameMessage");
+});
