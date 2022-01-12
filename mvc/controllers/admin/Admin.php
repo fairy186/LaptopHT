0dac83862483e5cd6a62b5ff1c32a4f2c4ea3e9c
Ngô Xuân Huy - 60135752 <huy.nx.60cntt@ntu.edu.vn>
2022-01-12 14:58:36 +0700

    fix

--- mvc/controllers/admin/Admin.php
+++ mvc/controllers/admin/Admin.php
@@ -17,45 +17,4 @@ class Admin extends Controller
         $this->data['dAdmin'] = $this->dAdmin->Get();
         $this->view("AdminLayout", $this->data);
     }
-    function Add()
-    {
-        $this->data["page"] = "AddAdmin";
-        $this->data['title'] = "Thêm quản trị viên";
-        $this->data['action'] = "Add";
-        if (isset($_POST['sm'])) {
-            // validate  check = 1 nếu tất cả các input đều đúng
-            $check = $this->dAdmin->CheckFirstName($_POST['firstname'])[0] && $this->dAdmin->CheckLastName($_POST['lastname'])[0];
-            if ($check)
-                $this->data["goDefault"] = $this->dAdmin->Add($_POST['firstname'], $_POST['lastname'], $_POST['account'], $_POST['password']);
-            else
-                $this->data["tb"] = "Lỗi";
-        }
-        $this->view("AdminLayout", $this->data);
-    }
-    function Edit($id)
-    {
-        $this->data["page"] = "EditAdmin";
-        $this->data['title'] = "Sửa quản trị viên";
-        $this->data['action'] = "Edit";
-        $this->data["admin"] = $this->dAdmin->GetByID($id);
-        if (isset($_POST['sm'])) {
-            $check = $this->dAdmin->CheckFirstName($_POST['firstname'])[0] && $this->dAdmin->CheckLastName($_POST['lastname'])[0];
-            if ($check)
-                $this->data["goDefault"] = $this->dAdmin->Edit($id, $_POST['firstname'], $_POST['lastname'], $_POST['account'], $_POST['password']);
-            else
-                $this->data["tb"] = "Lỗi";
-        }
-        $this->view("AdminLayout", $this->data);
-    }
-    function Delete($id)
-    {
-        $this->data["page"] = "DeleteAdmin";
-        $this->data['title'] = "Xóa quản trị viên";
-        $this->data['action'] = "Delete";
-        $this->data["admin"] = $this->dAdmin->GetByID($id);
-        if (isset($_POST['sm'])) {
-            $this->data["goDefault"] = $this->dAdmin->Delete($id);
-        }
-        $this->view("AdminLayout", $this->data);
-    }
 }
