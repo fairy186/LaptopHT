9f9fdc2f44b2116c984428b7f63b87e307c83cbe
Ngô Xuân Huy - 60135752 <huy.nx.60cntt@ntu.edu.vn>
2022-01-12 12:09:27 +0700

    update

--- mvc/views/layout/AdminLogin.php
+++ mvc/views/layout/AdminLogin.php
@@ -5,6 +5,7 @@
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="icon" href='<?php echo "/$data[domain]/images/shared/icon.jpg" ?>' type="image/x-icon">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
@@ -35,14 +36,14 @@
                   }).change(function() {
                         check(this, "<?php echo $data['domain'] ?>", "Customer");
                   });
-                  
+
             });
       </script>
       <script>
-          if (window.history.replaceState) {
-               window.history.replaceState(null, null, window.location.href);
-          }
-     </script>
+            if (window.history.replaceState) {
+                  window.history.replaceState(null, null, window.location.href);
+            }
+      </script>
 </body>
 
 </html>
--- mvc/views/layout/ClientLayout.php
+++ mvc/views/layout/ClientLayout.php
@@ -12,13 +12,15 @@ if (isset($_SESSION['notify'])) {
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title><?php echo $data['title']; ?></title>
+     <link rel="icon" href='<?php echo "/$data[domain]/images/shared/icon.jpg"?>' type="image/x-icon">
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
      <style>
-          p,.row {
+          p,
+          .row {
                margin: 0px;
                padding: 0px;
           }
@@ -38,7 +40,8 @@ if (isset($_SESSION['notify'])) {
                padding: 0;
                z-index: 100;
           }
-          .btn:hover{
+
+          .btn:hover {
                filter: drop-shadow(0 0 10px #fff);
           }
 
@@ -61,7 +64,7 @@ if (isset($_SESSION['notify'])) {
                     <div class="collapse navbar-collapse">
                          <div class="mb-0 mx-auto">
                               <div class="input-group">
-                                   <input id="search" class="form-control border-2 border-primary" name="info" value="<?php echo @$data['info']?>" type="search" onsearch="search()" placeholder="Tìm kiếm" aria-label="Search">
+                                   <input id="search" class="form-control border-2 border-primary" name="info" value="<?php echo @$data['info'] ?>" type="search" onsearch="search()" placeholder="Tìm kiếm" aria-label="Search">
                                    <button class="btn btn-outline-light border-2 border-primary" type="button" onclick="search()"><i class="bi bi-search"></i></button>
                               </div>
                          </div>
